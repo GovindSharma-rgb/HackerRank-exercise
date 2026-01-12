0da817bc8534b1c78d54de248e886df4468f2240
Govind Sharma <govindsharma043136@gmail.com>
2026-01-12 22:17:06 +0530

    first commit

---
+++ Hello_World.c
@@ -0,0 +1,26 @@
+/*Write a C program to print the message: Hello World!
+
+Input Format
+
+No input is required.
+
+Constraints
+
+N/A
+
+Output Format
+
+Single line printing exactly: Hello World!*/
+
+//-------------------------------------------------Answers---------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+   printf("Hello World!");    
+    return 0;
+}
